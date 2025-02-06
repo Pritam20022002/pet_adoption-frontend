4460a9c7b42feb4e6792fe3466fe097b9f760268
Pritam Dey <108470372+Pritam20022002@users.noreply.github.com>
2025-02-07 00:28:05 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+document.getElementById("login-form").addEventListener("submit", async function (e) {
+    e.preventDefault();
+
+    const mobile_number = document.getElementById("mobile_number").value;
+    const password = document.getElementById("password").value;
+
+    try {
+        const response = await fetch("https://petadopt-jv77.onrender.com/login", {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({ mobile_number, password }),
+        });
+
+        const data = await response.json();
+
+        if (response.ok) {
+            sessionStorage.setItem("userId", data.userId); // Store user ID in sessionStorage
+            window.location.href = "home/home.html"; // Redirect to home page
+        } else {
+            // Handle error response from server
+            document.getElementById("error-message").innerText = data.message || "Login failed";
+        }
+    } catch (error) {
+        // Handle network errors
+        document.getElementById("error-message").innerText = "An error occurred. Please try again later.";
+    }
+});
